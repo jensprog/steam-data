ff289d6f90cbbdefa71b659d29bef9f7aee0719b
Jens <jk224vx@student.lnu.se>
2026-04-10 12:39:14 +0200

    Updated css for resource list and pagination links for better visiblity and design.

--- app/components/PaginationLinks.vue
+++ app/components/PaginationLinks.vue
@@ -7,8 +7,20 @@ defineProps({
 </script>
 
 <template>
-  <div class="pagination-links flex justify-center gap-4 mt-4">
-    <NuxtLink v-if="prev" :to="prev" class="pagination-link">Previous Page</NuxtLink>
-    <NuxtLink v-if="next" :to="next" class="pagination-link">Next Page</NuxtLink>
+  <div class="flex justify-center gap-4 mt-6">
+    <NuxtLink
+      v-if="prev"
+      :to="prev"
+      class="bg-[#1b2838] text-[#66c0f4] border border-[#2a475e] hover:border-[#66c0f4] py-2 px-5 rounded transition-colors duration-200"
+    >
+      &larr; Previous
+    </NuxtLink>
+    <NuxtLink
+      v-if="next"
+      :to="next"
+      class="bg-[#1b2838] text-[#66c0f4] border border-[#2a475e] hover:border-[#66c0f4] py-2 px-5 rounded transition-colors duration-200"
+    >
+      Next &rarr;
+    </NuxtLink>
   </div>
 </template>
--- app/components/ResourceList.vue
+++ app/components/ResourceList.vue
@@ -9,17 +9,23 @@ defineProps({
 </script>
 
 <template>
-  <div>
-    <table class="table-auto mx-auto lg:text-xl border-collapse">
+  <div class="overflow-x-auto mt-6">
+    <table class="table-auto mx-auto w-full max-w-2xl border-collapse">
       <thead>
-        <tr>
-          <th class="border border-gray-400 py-2 px-4">Name</th>
+        <tr class="bg-[#1b2838]">
+          <th class="text-left text-[#66c0f4] font-semibold py-3 px-6 border-b border-[#2a475e]">Name</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in items" :key="item.id">
-          <td class="border border-gray-400 py-2 px-4">
-            <NuxtLink :to="`${basePath}/${item.id}`">{{ item.name }}</NuxtLink>
+        <tr
+          v-for="item in items"
+          :key="item.id"
+          class="border-b border-[#2a475e] hover:bg-[#1b2838] transition-colors duration-150"
+        >
+          <td class="py-3 px-6">
+            <NuxtLink :to="`${basePath}/${item.id}`" class="text-[#c7d5e0] hover:text-[#66c0f4] transition-colors duration-150">
+              {{ item.name }}
+            </NuxtLink>
           </td>
         </tr>
       </tbody>
