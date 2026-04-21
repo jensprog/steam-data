b2e63185d5c04f6d8aecc58c12ed62ac48c891ea
Jens <jk224vx@student.lnu.se>
2026-04-21 14:12:47 +0200

    Reimplemented the public url for client to browser requests.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
 
     runtimeConfig: {
+        apiBaseUrl: "http://localhost:8000",
         public: {
             apiBaseUrl: "http://localhost:8000",
         },
