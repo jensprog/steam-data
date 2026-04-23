7f0959a3552b926e62f1792ae9069215592a7afa
Jens <jk224vx@student.lnu.se>
2026-04-23 19:18:02 +0200

    Added suspense to the dashboard to wait for all components to load and in the meantime showing a message while waiting.

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -6,11 +6,18 @@ import DeveloperGameCountTable from "~/components/DeveloperGameCountTable.vue";
 <template>
     <div>
         <h1 class="text-2xl font-bold mb-6 text-center">Dashboard</h1>
-        <div class="grid grid-cols-2 gap-6">
-            <PriceChart />
-            <GameOwnerBar />
-            <GenreGameCountTable />
-            <DeveloperGameCountTable />
-        </div>
+        <Suspense>
+            <template #default>
+                <div class="grid grid-cols-2 gap-6">
+                    <PriceChart />
+                    <GameOwnerBar />
+                    <GenreGameCountTable />
+                    <DeveloperGameCountTable />
+                </div>
+            </template>
+            <template #fallback>
+                <div class="text-[#c7d5e0] text-center mt-10">Loading dashboard...</div>
+            </template>
+        </Suspense>
     </div>
 </template>
